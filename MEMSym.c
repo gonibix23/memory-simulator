e7d0f9aabd0bcf306f486292e5b7a036329c6074
Gonzalo de Antonio <47611628+gonibix23@users.noreply.github.com>
2021-11-30 02:07:46 +0100

    Parseo de direcciones de memoria
    No está bien la función de extraer los bits de un integer hay que echarle un ojo.

--- MEMSym.c
+++ MEMSym.c
@@ -13,6 +13,7 @@ void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque);
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
 void VolcarCACHE(T_CACHE_LINE *tbl);
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
+int ExtraerBits(int addr, int posicion, int numBits);
 
 int main(int argc, char *argv){
     int globaltime = 0;
@@ -28,7 +29,7 @@ int main(int argc, char *argv){
         printf("ERROR: No existe el archivo");
         return -1;
     }
-    fread(&Simul_RAM,sizeof(Simul_RAM),1,contenidoRAM);
+    fread(&Simul_RAM,sizeof(Simul_RAM),sizeof(unsigned char),contenidoRAM);
     fclose(contenidoRAM);
 
     FILE *memorias;
@@ -37,9 +38,13 @@ int main(int argc, char *argv){
         printf("ERROR: No existe el archivo");
         return -1;
     }
-    fclose(memorias);
-    
+    unsigned int addr;
+    while (fscanf(memorias, "%X", &addr) != EOF) {
+        int ETQ, palabra, linea, bloque;
+        ParsearDireccion(addr, &ETQ, &palabra, &linea, &bloque);
+    }
     VolcarCACHE(cache);
+    fclose(memorias);
 }
 
 void IniciarCache(T_CACHE_LINE *cache){
@@ -56,14 +61,19 @@ void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque){
 }
 
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque){
-
+    ExtraerBits(0x800, 0, 0);
+    *ETQ = ExtraerBits(addr, 0, 5);
+    *linea = ExtraerBits(addr, 6, 3);
+    *palabra = ExtraerBits(addr, 9, 4);
+    *bloque = ExtraerBits(addr, 0, 8);
+    printf("PRUEBA PARSEO - ADDR: %04X ETQ: %02X Linea: %02X Palabra: %02X Bloque: %02X\n", addr, *ETQ, *palabra, *linea, *bloque);
 }
 
 void VolcarCACHE(T_CACHE_LINE *tbl){
     for (int i = 0; i < NUM_FILAS; i++){
-        printf("ETQ:%x\tData", tbl->ETQ);
-        for (int j = 0; j < TAM_LINEA; j++){
-            printf(" %x", tbl->Data[j]);
+        printf("ETQ:%X\tData", tbl->ETQ);
+        for (int j = TAM_LINEA-1; j >= 0 ; j--){
+            printf(" %X", tbl->Data[j]);
         }
         printf("\n");
     }
@@ -71,4 +81,9 @@ void VolcarCACHE(T_CACHE_LINE *tbl){
 
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
 
+}
+
+int ExtraerBits(int addr, int posicion, int numBits){
+    printf("\t%d\t||", (((1 << numBits) - 1) & (addr >> (posicion - 1))));
+    return (((1 << numBits) - 1) & (addr >> (posicion - 1)));
 }
